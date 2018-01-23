64c19d364f0dcf5c43e4748b07dbaf262f357af2
JarodBoone <jtboone12@gmail.com>
2018-01-23 13:01:14 -0500

    created gulpfile switch to sass

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+//gulp
+var gulp = require('gulp'); 
+
+//gulp plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+//lint task
+gulp.task('hint',function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default')); 
+});
+
+
+//compile sass into css and store in dist/css
+gulp.task('sass',function() { 
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css')); 
+}); 
+
+// Concatenate and minify javascript and store in dist/js 
+gulp.task('scripts',function() { 
+    return gulp.src('js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js')); 
+}); 
+
+// setup watch loop
+gulp.task('watch',function() { 
+    gulp.watch('js/*.js',['hint','scripts']); 
+    gulp.watch('scss/*scss',['sass']); 
+});
+
+// default task 'gulp'
+gulp.task('default',['hint','scripts','sass','watch']); 
+
+
+
+
