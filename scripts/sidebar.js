ff3a960d1766af0b0bc5726dcdb6ea08938ed5e3
JarodBoone <jtboone12@gmail.com>
2018-01-23 16:40:26 -0500

    separated parameter parsing function

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
-
-    // function to get url query parameters by query name 
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
+    
     var t1 = $('.display.item.t1');
     var t2 = $('.display.item.t2');
     var t3 = $('.display.item.t3');
