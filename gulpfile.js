e2303ac31d592c53495113a98225cff1d92a5f0d
JarodBoone <jtboone12@gmail.com>
2018-01-25 15:06:04 -0500

    updated gulpfile to clean css compilation

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,13 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean'); 
 
+//gulp-sass is compiling css files for every scss file even though I import all my 
+//scss files in main.scss (and therefore they are already in main.css). You're out of you're 
+//godamn mind if you think I'm going to try and debug a wrapper of a wrapper of a sass port
+//just going to grossly clean an array of css file names
+var killMe = ['dist/gol.css','dist/mixin.css','scss/useless.scss'];
 
 //lint task
 gulp.task('hint',function() {
@@ -16,12 +22,15 @@ gulp.task('hint',function() {
         .pipe(jshint.reporter('default')); 
 });
 
-
 //compile sass into css and store in dist/css
 gulp.task('sass',function() { 
-    return gulp.src('scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
-        .pipe(gulp.dest('dist')); 
+     gulp.src('scss/*.scss')
+        .pipe(sass({outputStyle: 'compressed', file: 'main.scss'}).on('error',sass.logError))
+        .pipe(gulp.dest('dist'))
+        .on('end',function() { 
+            gulp.src(killMe, { read: false, base: 'dist/' })
+                .pipe(clean({ force: true }));
+        });
 }); 
 
 // Concatenate and minify javascript and store in dist/js 
