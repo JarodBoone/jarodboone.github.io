2079d2e6e68c9d8a9ebe69ef74fd2f6c3c852b25
JarodBoone <jtboone12@gmail.com>
2018-01-25 20:43:00 -0500

    added gameboard resizing

--- scripts/game_of_life.js
+++ scripts/game_of_life.js
@@ -17,6 +17,9 @@
         width: 0, 
         height: 0,
 
+        // height percentage for resizing
+        hRatio: 0,
+
         numberOfBlocks: 75,
 
         blockHeight: 0,
@@ -622,10 +625,65 @@
 
         },
 
+        //fucntion to resize the game board based on the gol_container dimensions 
+        resize : function () { 
+            // copy the state to reapply 
+            var temp = this.state.slice(); 
+            this.clean(); 
+            this.board.remove(); 
+
+            this.height = Math.floor($('.gol_container').parent().height() * this.hRatio); 
+            this.width = this.height;
+            this.blockHeight = this.height / this.numberOfBlocks;
+            this.blockWidth = this.width / this.numberOfBlocks;
+
+            // have to use jquery to set the game_board elements for some reason because
+            // D3 does not want to cooperate 
+            $('.game_board').css({
+                "width": this.width,
+                "height": this.height
+            });
+
+            this.board = d3.select('.game_board').append('svg');
+
+            // size the game board to the given width and height
+            this.board.attr('width', this.width)
+                .attr('height', this.height);
+
+            // create the grid
+            for (var row = 0; row <= this.numberOfBlocks; row++) {
+
+                this.board.append('svg:line')
+                    .attr('x1', 0)
+                    .attr('y1', row * this.blockHeight)
+                    .attr('x2', this.width)
+                    .attr('y2', row * this.blockHeight)
+                    .style("stroke", "rgba(95, 95, 95, 0.178)")
+                    .style("stroke-width", 1);
+
+                this.board.append('svg:line')
+                    .attr('x1', row * this.blockWidth)
+                    .attr('y1', 0)
+                    .attr('x2', row * this.blockWidth)
+                    .attr('y2', this.height)
+                    .style("stroke", "rgba(95, 95, 95, 0.178)")
+                    .style("stroke-width", 1);
+
+            } 
+
+            //alert(temp.length); 
+
+            for (var i = 0; i < temp.length; i++) { 
+                this.populate(temp[i].x,temp[i].y); 
+            }
+
+        }, 
+
         // need to call this function before any the board is used 
         init : function () {
             // set dimensions of the game board
-            this.height = $('.gol_container').parent().height() * $('.gol_container').height()/100; 
+            this.hRatio = $('.gol_container').height() / 100; 
+            this.height = Math.floor($('.gol_container').parent().height() * this.hRatio); 
             this.width = this.height; 
 
             // determine the blockHeight and block width
@@ -684,10 +742,16 @@
             $('#single').css({'color':'orange'}).attr({'data-on': 1}); 
             this.loadState(null); 
 
+            $(window).resize({gol: this}, function(event) {  
+                gol.resize(); 
+                //alert(gol.height); 
+            });
+
         }
     };
 
-    gol.init(); 
+    var a = gol.init(); 
+    gol.resize(); 
 
 }());
 
