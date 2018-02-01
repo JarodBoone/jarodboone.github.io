38699613b0e198a5b19056e478d6f3ccfcaaa9de
JarodBoone <jtboone12@gmail.com>
2018-01-31 19:06:04 -0500

    im pretty stupid...

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,6 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean'); 
 
-//gulp-sass is compiling css files for every scss file even though I import all my 
-//scss files in main.scss (and therefore they are already in main.css). You're out of you're 
-//godamn mind if you think I'm going to try and debug a wrapper of a wrapper of a sass port
-//just going to grossly clean an array of css file names
-var killMe = ['dist/gol.css','dist/mixin.css','scss/useless.scss'];
-
 //lint task
 gulp.task('hint',function() {
     return gulp.src('scripts/*.js')
@@ -24,13 +18,9 @@ gulp.task('hint',function() {
 
 //compile sass into css and store in dist/css
 gulp.task('sass',function() { 
-     gulp.src('scss/*.scss')
+     gulp.src('scss/main.scss')
         .pipe(sass({outputStyle: 'compressed', file: 'main.scss'}).on('error',sass.logError))
-        .pipe(gulp.dest('dist'))
-        .on('end',function() { 
-            gulp.src(killMe, { read: false, base: 'dist/' })
-                .pipe(clean({ force: true }));
-        });
+        .pipe(gulp.dest('dist'));
 }); 
 
 // Concatenate and minify javascript and store in dist/js 
